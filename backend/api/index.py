801e08c0daaaa4348673d7e5a5d1459400646f0c
Kevin Burke <kevinburke@Kevins-MacBook-Pro-2.local>
2026-05-29 13:32:37 -0400

    fix: look one level up to import app file in api/index.py

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,4 +1,7 @@
-from main import app
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from app import app
 from mangum import Mangum
 
 handler = Mangum(app)
